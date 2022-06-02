4bbdab6735105b574a1f1567ba2ca8b965daa660
Aristya14 <72466039+Aristya14@users.noreply.github.com>
2022-06-02 20:31:51 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Models\Category;
+use App\Models\Author;
+use App\Models\Publisher;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +17,13 @@ use App\Http\Controllers\BookController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', [BookController::class, 'index'])->name('home');
 Route::get('/show/{id}', [BookController::class, 'show'])->name('book.show');
+Route::get('/create', [BookController::class, 'create'])->name('book.create');
+Route::delete('/delete/{id}', [BookController::class, 'destroy'])->name('book.destroy');
 
 Route::get('/lang={locale}', 'App\Http\Controllers\LocalizationController@index');
 
@@ -31,4 +35,46 @@ Route::get('/login/lang={locale}', 'App\Http\Controllers\LocalizationController@
 Route::get('/register', function(){
     return view('register');
 });
+Route::get('/category', function(Category $category){
+    return view('category', [
+        'title' => "All Categories",
+        'data' => Category::all(),
+        'jenis' => "category",
+    ]);
+})->name('all.categories');
+
+Route::get('/author', function(Author $author){
+    return view('category', [
+        'title' => "All Authors",
+        'data' => Author::all(),
+        'jenis' => "author",
+    ]);
+})->name('all.authors');
+
+Route::get('/publisher', function(Publisher $publisher){
+    return view('category', [
+        'title' => "All Publishers",
+        'data' => Publisher::all(),
+        'jenis' => "publisher",
+    ]);
+})->name('all.publishers');
 Route::get('/register/lang={locale}', 'App\Http\Controllers\LocalizationController@index');
+
+Route::get('/author/{author:id}', function(Author $author) {
+    return view('home', [
+        'title' =>"Books by ".$author->name,
+        'data' => $author->books,
+    ]);
+});
+Route::get('/publisher/{publisher:id}', function(Publisher $publisher) {
+    return view('home', [
+        'title' =>"Books by ".$publisher->name,
+        'data' => $publisher->books,
+    ]);
+});
+Route::get('/category/{category:id}', function(Category $category) {
+    return view('home', [
+        'title' =>"Books by ".$category->name,
+        'data' => $category->books,
+    ]);
+});
