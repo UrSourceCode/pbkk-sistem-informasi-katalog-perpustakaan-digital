857feecfbe5aa2c72fe5f334ad8b25752cf316bc
HaniifAJ <haniif02aj@gmail.com>
2022-06-07 11:56:43 +0700

    mailing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,16 @@
 
 namespace App\Console;
 
+use App\Console\Commands\emailUser;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $command = [
+        emailUser::class,
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +20,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('email:notify-user')
+        ->weekly();
     }
 
     /**
